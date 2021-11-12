84a2d287b94362a120d4b3fb567a8ff885485b22
Alexandra Weaver <aweaver0124@gmail.com>
2021-11-12 15:28:29 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const recipient = [];
+
+function writeCards(recipient, event) {
+    for (let i = 0; i < recipient.length; i++) {
+
+        recipient.splice(i, 1, `Thank you, ${recipient[i]}, for the wonderful ${event} gift!`)
+        debugger;
+    }
+    return recipient
+}
+writeCards(["Tom", "Maddi", "Trace"], "Christmas");
+
+
+function countDown(x) {
+    while (x >= 0) {
+        console.log(x--)
+    }
+  }
